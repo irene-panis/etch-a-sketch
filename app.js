fde0c5dae6ed5157bf2768427f93cd17f0dfccad
Irene Panis <panisirene@gmail.com>
2022-10-09 02:24:26 -0700

    Create grid methods and edit dimensions

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const container = document.querySelector('.container');
-const divs = document.querySelectorAll('div.container');
+const WIDTH = 500;
 
-function createGrid() {
+// create initial 16x16 grid
+function initialize() {
   for (i = 0; i < 256; i++) {
     const square = document.createElement('div');
+    square.classList.add('square');
     container.appendChild(square);
+    square.addEventListener('mouseover', () => {
+      square.classList.add('written');
+    });
   }
 }
 
-divs.forEach((div) => {
-  div.addEventListener('mouseover', () => {
-    div.classList.add('written');
-  });
-});
+function createGrid(numSquares) {
+  for (i = 0; i < numSquares*numSquares; i++) {
+    const square = document.createElement('div');
+    square.style.width = `${calcSide(numSquares)}px`;
+    square.style.height = `${calcSide(numSquares)}px`;
+    square.classList.add('square');
+    container.appendChild(square);
+    square.addEventListener('mouseover', () => {
+      square.classList.add('written');
+    });
+  }
+}
 
+function calcSide(numSquares) {
+  return WIDTH / numSquares;
+}
 
-createGrid();
+function resetGrid() {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    container.removeChild(square);
+  })
+}
+
+initialize();
