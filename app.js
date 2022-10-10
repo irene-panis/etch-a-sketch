4e78342dfdd466427d2e74400fa572c7f9cdc66b
Irene Panis <panisirene@gmail.com>
2022-10-10 02:32:59 -0700

    Improve sidebar functionality and fix bugs

--- app.js
+++ app.js
@@ -58,26 +58,70 @@ BUTTON FUNCTIONS
 const size = document.querySelector('#size');
 const color = document.querySelector('#color');
 const erase = document.querySelector('#erase');
-const sidebar = document.querySelector('.customize');
-const sizeSlider = document.getElementById('#size-slider');
+const sidebar = document.querySelector('.sidebar');
+const customize = document.querySelector('.customize');
+
+function makeWindow() {
+  sidebar.innerHTML = `
+  <div class="window">
+    <div class="title-bar">
+      <div class="left">
+        <div class="window-title">ALERT!</div>
+      </div>
+      <div class="right">
+        <div id="x">X</div>
+      </div>
+    </div>
+    <div class="customize" style="background-color: oldlace;"></div>
+  </div>
+  `;
+}
+
+
 
 size.addEventListener('click', () => {
-  let value = 16;
-  let total = count();
-  if (total !== 256) { value = Math.sqrt(total)};
-  sidebar.innerHTML = `
-    <div class="prompt">Choose your pen thickness:</div>
-    <div class="slider-container">
-      <img src="img/small.jpg" class="pen" width=20px height=20px>
-      <input type="range" min="10" max="100" value="${value}" class="slider" id="size-slider">
-      <img src="img/big.jpg" class="pen" width=20px height=20px>
-    </div>
-  `;
+  makeWindow();
+  customizeSize();
 });
+
+function customizeSize() {
+  let total = count();
+  let value = Math.sqrt(total);
+  const customize = document.querySelector('.customize');
+  customize.innerHTML = `
+  <div class="prompt">Choose your pen thickness (this will clear your drawing!):</div>
+  <div class="slider-container">
+    <img src="img/small.jpg" class="pen" width=20px height=20px>
+    <input type="range" min="10" max="100" value="${value}" class="slider" id="size-slider">
+    <img src="img/big.jpg" class="pen" width=20px height=20px>
+  </div>
+  <button id="select-size">Select</button>
+`;
+}
+
+// variables to detect slider value changes
+let storedValue = 16;
+let newValue = 16;
 
 document.addEventListener('change', (e) => {
   if (e.target && e.target.id == 'size-slider') {
-    createGrid(e.target.value);
+    newValue = e.target.value;
+  }
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target && e.target.id == 'select-size') {
+    if (newValue !== storedValue) { // checks if slider value has been changed
+      createGrid(newValue); // if new slider value present, clear the grid
+      storedValue = newValue; // then store new slider value
+    } // otherwise, if slider value has stayed the same,
+    clearSidebar(); // simply clear the sidebar w/o clearing grid
+  }
+})
+
+document.addEventListener('click', (e) => {
+  if (e.target && e.target.id == 'x') {
+    clearSidebar();
   }
 });
 
